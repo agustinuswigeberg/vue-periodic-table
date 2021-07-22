7aada190f9ecadcb52764d27753b6ba8e242c06b
agustinuswigeberg <agustinus.wigeberg@gmail.com>
2021-07-22 19:48:09 +0200

    Add invert feature

--- src/components/PeriodicTable.vue
+++ src/components/PeriodicTable.vue
@@ -1,13 +1,34 @@
 <template>
   <div>
     <div class="container-fluid">
-      <table>
-        <tr v-for="(elementsRow, rowIndex) in mappedElements" :key="rowIndex">
-          <td v-for="(element, colIndex) in elementsRow" :key="colIndex">
-            <PeriodicTableElement v-if="element" :element="element" />
-          </td>
-        </tr>
-      </table>
+      <div class="row">
+        <div class="col-12">
+          <table>
+            <tr
+              v-for="(elementsRow, rowIndex) in mappedElements"
+              :key="rowIndex"
+            >
+              <td v-for="(element, colIndex) in elementsRow" :key="colIndex">
+                <PeriodicTableElement v-if="element" :element="element" />
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
+      <div class="col-12">
+        <div class="custom-control">
+          <input
+            type="checkbox"
+            class="custom-control-input"
+            id="cb-invert-table"
+            v-model="invert"
+            @change="mapElementsToTable"
+          />
+          <label for="cb-invert-table" class="custom-control-label">
+            INVERT
+          </label>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -33,18 +54,45 @@ export default {
   data() {
     return {
       mappedElements: [],
+      invert: false,
     };
   },
   mounted() {
-    while (this.mappedElements.push(new Array(numOfColumns)) < numOfRows);
     this.mapElementsToTable();
   },
   methods: {
     mapElementsToTable() {
-      this.elements.forEach((element) => {
-        this.mappedElements[element.row - 1][element.column - 1] = element;
-      });
+      this.mappedElements = [];
+      while (this.mappedElements.push(new Array(numOfColumns)) < numOfRows);
+
+      if (this.invert) {
+        this.elements.forEach((element) => {
+          this.mappedElements[numOfRows - element.row][element.column - 1] =
+            element;
+        });
+      } else {
+        this.elements.forEach((element) => {
+          this.mappedElements[element.row - 1][element.column - 1] = element;
+        });
+      }
     },
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.custom-control {
+  display: block;
+  margin-top: 20px;
+  font-size: 1.2rem;
+  text-align: center;
+
+  .custom-control-input {
+    margin-right: 6px;
+  }
+
+  .custom-control-label {
+    font-weight: bold;
+  }
+}
+</style>
